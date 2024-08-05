eb859a401b6b7db007eeb6bdfde54405abaf0933
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-05 17:03:24 +0200

    Document ProductAllocationProhibition header file

--- MainSource/ProductAllocationProhibition.h
+++ MainSource/ProductAllocationProhibition.h
@@ -12,43 +12,135 @@
 #include "Cell.h"
 using namespace std;
 
-
-
+/**
+ * @brief The class ProductAllocationProhibition handles data about the positions
+ * in which the products cannot be allocated.  
+ */
 class ProductAllocationProhibitions{
     private:
-        Product product;						///< Product to be allocated 
-        std::vector<Shelf> forbiddenShelves;			///< Shelves where the product can not be stored
+        Product product;						    ///< Product to be allocated 
+        std::vector<Shelf> forbiddenShelves;		///< Shelves where the product can not be stored
         std::vector<Cell> forbiddenCells;			///< Shelves where the product can not be stored 
         std::vector<Block> forbiddenBlocks;			///< Blocks where the product can not be stored 
         static void recoverWarehouseData(Warehouse &warehouse,std::map<std::string, Cell> & cellsByCode, 
                                         std::map<long int, Shelf> &shelvesById, std::map<std::string, Block> & blocksByName);
     public:
+        /**
+         * @brief Constructor.
+         */
         ProductAllocationProhibitions();
+
+        /**
+         * @brief Copy constructor. 
+         * @param other The object to be copied. 
+         */
         ProductAllocationProhibitions(const ProductAllocationProhibitions &other);
+
+        /**
+         * @brief Constructor. 
+         * @param product The product with an allocation prohibition. 
+         * @param forbiddenShelves The list of forbidden shelves.
+         * @param forbiddenCells The list of forbidden cells.
+         * @param block The list of blocks.
+         */
         ProductAllocationProhibitions(Product &product, std::vector<Shelf>forbiddenShelves, std::vector<Cell> forbiddenCells, 
-                                     std::vector<Block> &block);
+                                     std::vector<Block> &blocks);
     
+        /**
+         * @brief Set the product with an allocation prohibition.
+         * @param other The product.
+         */ 
         void setProduct(Product & other);
+
+        /**
+         * @brief Set the forbidden shelves. 
+         * @param others The list of forbiddend shelves.
+         */
         void setForbiddenShelves(std::vector<Shelf> & others);
+
+        /**
+         * @brief Set the forbidden cells.
+         * @param others The list of fobrbidden cells. 
+         */
         void setForbiddenCells(std::vector<Cell> &others);
+
+        /**
+         * @brief Set the forbidden blocks. 
+         * @param others The list of forbidden blocks. 
+         */
         void setForbiddenBlocks(std::vector<Block> &others);
     
+        /**
+         * @brief Get the product with an allocation prohibition.
+         * @return The product with an allocation prohibition.
+         */
         Product getProduct() const; 
+        
+        /**
+         * @brief Get the list of forbidden shelves. 
+         * @return The list of forbidden shelves. 
+         */
         std::vector<Shelf> & getForbiddenShelves();
+
+        /**
+         * @brief Get the list of forbidden cells. 
+         * @return THe list of forbidden cells.
+         */
         std::vector<Cell> & getForbiddenCells();
+
+        /**
+         * @brief Get the list of forbidden blocks.
+         * @return The list of forbidden blocks. 
+         */
         std::vector<Block> & getForbiddenBlocks();
     
+        /**
+         * @brief Add one cell to the prohibition list.
+         * @param cell The cell to be prohibited. 
+         */
         void addProhibition(Cell &cell);
+
+        /**
+         * @brief Add one shelf to the prohibition list. 
+         * @param shelf The shelf to be prohibited. 
+         */
         void addProhibition(Shelf &shelf);
-        void addProhibition(Block &);
-    
+
+        /**
+         * @brief Add one block to the prohibition list. 
+         * @param block The probhition list. 
+         */
+        void addProhibition(Block &block);
+
+        /**
+         * @brief Remove a cell from the prohibition list. 
+         * @param i The index of the cell to be removed. 
+         */
         void removeProductCellProhibition(int i);
     
+        /**
+         * @brief Remove a shelf from the prohibition list. 
+         * @param i The index of the shelf to be removed.
+         */
         void removeProductShelfProhibition(int i);
     
+        /**
+         * @brief Remove block from the prohibition list. 
+         * @param i The index of the block to be removed. 
+         */
         void removeProductBlockProhibition(int i);
-    
+        
+        /**
+         * @brief Read prohibition data from a file. 
+         * @param fileName The file name.
+         * @return A list containing all the prohibitions. 
+         */
         static std::vector<ProductAllocationProhibitions> readAllProhibitionsData(std::ifstream &fileName);
 		
+        /**
+		 * @brief Assignment operator override. 
+		 * @param other The operator right side object. 
+		 * @return A reference to the modified object.
+         */
 		ProductAllocationProhibitions & operator=(const ProductAllocationProhibitions &other); 
 };
