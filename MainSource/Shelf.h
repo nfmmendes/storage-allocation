287225b61538e73e9749efff2e423025ea76730b
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-05 17:54:31 +0200

    Refactor references to std containers

--- MainSource/Shelf.h
+++ MainSource/Shelf.h
@@ -4,33 +4,36 @@
 #include<vector>
 #include<utility>
 #include "Cell.h" 
+using std::vector;
+using std::pair;
+using std::string;
 
 class Shelf{
 
     private:
-        long int Id;							    ///< Shelf id
-        std::vector<Cell> cells;         		    ///< Cells inside the shelf TODO: Make this relationship be consistent in both senses 
-        std::pair<double,double> bottomLeftCoords;	///< Coordinates of shelf the bottom left extremity 
-        std::string blockName;					    ///< Name of block where the shelf is 
-        int columns;							    ///< Number of columns in the shelf
-        int rows; 								    ///< Number of rows in the shelf 
-        double cellLength;						    ///< Shelf cells length (It is supposed that the cells have all the same size)
-        double cellWidth; 						    ///< Shelf cells width (It is supposed that the cells have all the same size)
+        long int Id;							///< Shelf id
+        vector<Cell> cells;         		    ///< Cells inside the shelf TODO: Make this relationship be consistent in both senses 
+        pair<double,double> bottomLeftCoords;	///< Coordinates of shelf the bottom left extremity 
+        string blockName;					    ///< Name of block where the shelf is 
+        int columns;							///< Number of columns in the shelf
+        int rows; 								///< Number of rows in the shelf 
+        double cellLength;						///< Shelf cells length (It is supposed that the cells have all the same size)
+        double cellWidth; 						///< Shelf cells width (It is supposed that the cells have all the same size)
         
         
     public:
         Shelf();
         Shelf(const Shelf &other);
         
-        Shelf(long int Id, std::vector<Cell> cells, std::pair<double, double> bottomLeftCoords, std::string blockName,
+        Shelf(long int Id, vector<Cell> cells, pair<double, double> bottomLeftCoords, string blockName,
               int columns, int rows, double cellLength, double cellWidth);
     
         long int getId() const;
         double getBottomLeftCoordX() const;
         double getBottomLeftCoordY() const;
-        std::pair<double,double> getBottomLeftCoords()const;
-        std::vector<Cell> getCells();
-        std::string getBlockName() const;
+        pair<double,double> getBottomLeftCoords()const;
+        vector<Cell> getCells();
+        string getBlockName() const;
         int getNumColumns() const;
         int getNumRows() const;
         double getCellLength() const;
@@ -38,9 +41,9 @@ class Shelf{
         
         void setBottomLeftCoordX(double value);
         void setBottomLeftCoordY(double value);
-        void setBottomLeftCoords(std::pair<double, double> value );
-        void setCells(std::vector<Cell> &cells);
-        void setBlockName(std::string name);
+        void setBottomLeftCoords(pair<double, double> value );
+        void setCells(vector<Cell> &cells);
+        void setBlockName(string name);
         void setNumColumns(int value);
         void setNumRows(int value);
         void setCellLength(double value);
