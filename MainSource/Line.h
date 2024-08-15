fd537b1bbbd9806ca97f054b482bdf98d0ffe5d8
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-15 22:58:38 +0200

    Document Line class header file

--- MainSource/Line.h
+++ MainSource/Line.h
@@ -8,8 +8,10 @@ using namespace std;
 
 namespace QuickTSP{
 
-    ///This class represent a basic line, with a starting point and a slope
-    ///in a tridimensional space
+    /**
+     * @brief Class <c> Line </c> Represent a basic rect segment, with a starting point and a 
+     * slope in a tridimensional space. 
+     */
     class Line{
         private: 
             Point startPoint;
@@ -17,20 +19,59 @@ namespace QuickTSP{
             std::string label; 
             
         public:
+            /**
+             * @brief Constructor. 
+             */
             Line();
         
-            ///Copy constructor
+            /**
+             * @brief Copy constructor. 
+             * @param other The object to be copied. 
+             */
             Line(const Line &other);
-                
-                ///Constructor member by member
+
+            /**
+             * @brief Constructor.
+             * @param label The line label.  
+             * @param pointA The starting point.
+             * @param pointB The ending point. 
+             */ 
             Line(std::string label, Point &pointA, Point &pointB);
                 
+            /**
+             * @brief Get the line slope. 
+             * @return The line slope. 
+             */
             Point getSlope() const;
+
+            /**
+             * @brief Get the line starting point. 
+             * @return The line starting point. 
+             */
             Point getStartPoint() const;
+
+            /**
+             * @brief Get the line label. 
+             * @return The line label. 
+             */
             std::string getLabel() const;
             
+            /**
+             * @brief Set the line slope.
+             * @param value The line slope. 
+             */
             void setSlope(Point &value);
+
+            /**
+             * @brief Get the starting point. 
+             * @param value The starting point. 
+             */
             void setStartPoint(Point &value);
+
+            /**
+             * @brief Set the line label.
+             * @param value The line label.
+            */
             void setLabel(std::string value);
     };
 }
