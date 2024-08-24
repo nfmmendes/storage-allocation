42f2013447a952cb6e1c3cbba0681077e2b72cf8
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-25 00:21:49 +0200

    Document class ExpeditionPoint

--- MainSource/ExpeditionPoint.h
+++ MainSource/ExpeditionPoint.h
@@ -6,24 +6,69 @@
 using std::string;
 using std::pair;
 
+/**
+ * @brief Class <c> Expedition Point </c> represents a delivery/expedition point in a warehouse. 
+ */
 class ExpeditionPoint{
 
 	private: 
-		string code;							///< Code of the expedition point 
-		string blockCode;						///< Code of block where is located the expedition point (or from where the products come) 
-		pair<double,double> coordinates;		///< Coordinates of expedition point 
+		string code;						///< Code of the expedition point.
+		string blockCode;					///< Code of block where is located the expedition point (or from where the products come).
+		pair<double,double> coordinates;	///< Coordinates of expedition point.
 	
 	public:
+		/**
+		 * @brief Constructor.
+		 */
 		ExpeditionPoint();
+
+		/**
+		 * @brief Constructor.
+		 * @param code Expedition point code.
+		 * @param blockCode Code of the block in which is located the expedition point.
+		 * @param coord The expedition point coordinate. 
+		 */
 		ExpeditionPoint(string &code, string &blockCode, pair<double,double> coord);
+
+		/**
+		 * @brief Copy constructor.
+		 * @param other The point to be copied. 
+		*/
 		ExpeditionPoint(const ExpeditionPoint &other);
 		
+		/**
+		 * @brief Set the expedition point code.
+		 * @param code The expedition point code. 
+		 */
 		void setCode(const string code);
+
+		/**
+		 * @brief Set the expedition point block code.
+		 * @param blockCode The expedition point block code. 
+		 */
 		void setBlockCode(const string blockCode);
+
+		/**
+		 * @brief Set the expedition point coordinate.
+		 * @param coordinates The expedition point coordinate. 
+		 */
 		void setCoordinates(const pair<double, double> &coordinates);
 		
+		/**
+		 * @brief Get the expedition point code. 
+		 * @return The expedition point code. 
+		 */
 		const string& getCode() const;
+
+		/**
+		 * @brief The expedition point block code.
+		 * @return The expedition point block code.
+		 */
 		const string& getBlockCode() const;
+
+		/**
+		 * @brief Get the expedition point coordinates.
+		 * @return The expedition point coordinates. 
+		 */
 		const pair<double,double>& getCoordinates() const;
-	
 };
