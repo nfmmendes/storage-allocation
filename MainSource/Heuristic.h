b0c8b47d958b1b6554ba60a042d03661b7740781
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-24 23:41:43 +0200

    Document class Heuristic

--- MainSource/Heuristic.h
+++ MainSource/Heuristic.h
@@ -3,15 +3,34 @@
 #include<vector>
 #include "NeighborhoodStructure.h"
 #include "AbstractSolution.h"
+using std::vector;
 
+/**
+ * @brief Class <c> Heuristic </c> is an abstract class that holds 
+ * the basic information and functions to represent a heuristic algorithm. 
+ */
 class Heuristic {
 
     protected: 
-        AbstractSolution *bestSolution;
-        AbstractSolution *currentSolution;
-        virtual bool StopCriteriaReached() =0;
-        std::vector<NeighborhoodStructure> neighborhoodStructures;
+        AbstractSolution *bestSolution;     ///< Best solution found by the heuristic. 
+        AbstractSolution *currentSolution;  ///< Heuristic current solution. 
+        vector<NeighborhoodStructure> neighborhoodStructures; ///< List of the heuristic neighborhood structures
+        
+        /**
+         * @brief Check if the heuristic stop criteria has been reached. 
+         * @return True if the stop critearia has been reached. False otherwise. 
+         */
+        virtual bool StopCriteriaReached() = 0;
+        
+        /**
+         * @brief Evaluate the solution considering the heuristic criteria. 
+         * @param solution The solution to be evaluated. 
+         */
         virtual void EvaluateSolution(AbstractSolution * solution)=0; 
-    public: 
+    public:
+        /**
+         * @brief Execute the heuristic. 
+         * @return Return the solution found by the heuristic. 
+         */ 
         virtual AbstractSolution * Execute() =0;
 };
