e8b087192e1ae1e6a62015ddcb5b9d83d43477c9
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-25 17:04:32 +0200

    Document inputData class

--- MainSource/InputData.h
+++ MainSource/InputData.h
@@ -15,39 +15,109 @@
 #include "IsolatedFamily.h"
 using std::vector; 
 
-
-//=======================================================================================
-//      This class allow to store all the input data of storage allocation problem
-//=======================================================================================
+/**
+ * @brief  Class <c> InputData </c> allows to store all the input data of the storage allocation problem
+ */
 class InputData{
     
+    //TODO: Correct this "singleton" implementation. 
     private:
-        static vector<Product> products;
-        static vector<Client> clients;
-        static vector<Order> orders;
-        static vector<Parameter> parameters;
-        static vector<ProductAllocationProhibitions> prohibitions;
-		static vector<IsolatedFamily> isolatedFamilies; 
-        static Warehouse warehouse;
-        bool alreadyCreated = false; 
+        static vector<Product> products;            ///< The list of products.
+        static vector<Client> clients;              ///< The list of clients. 
+        static vector<Order> orders;                ///< The list of orders. 
+        static vector<Parameter> parameters;        ///< The list of problem parameters. 
+        static vector<ProductAllocationProhibitions> prohibitions; ///< The list of allocation prohibitions. 
+		static vector<IsolatedFamily> isolatedFamilies; ///< THe list of isolated families. 
+        static Warehouse warehouse;                 ///< The warehouse layout.
+        bool alreadyCreated = false;                ///< Store if an object have been created already. 
     
     public:
+        /**
+         * @brief Constructor. 
+         */
         InputData();
+
+        /**
+         * @brief Constructor
+         * @param metadataFile Name of the file containing the input data.  
+         */
         InputData(string  metadataFile);
     
+        /**
+         * @brief Set the list of products. 
+         * @param products The list of products. 
+         */
         void setProducts(vector<Product> products);
+
+        /**
+         * @brief Set the list of clients. 
+         * @param clients The list of clients. 
+         */
         void setClients(vector<Client> clients);
+
+        /**
+         * @brief Set the list of orders. 
+         * @param orders The list of orders. 
+         */
         void setOrders(vector<Order> orders);
+
+        /**
+         * @brief Set the list of optimization parameters. 
+         * @param parameters The list of optimization parameters. 
+         */
         void setParameters(vector<Parameter> parameters);
+
+        /**
+         * @brief Set the warehouse layout. 
+         * @param warehouse The hourehouse layout. 
+         */
         void setWarehouse(Warehouse&& warehouse);
+
+        /**
+		 * @brief Set the list of isolated families. 
+		 * @param isolatedFamilies The list of isolated families. 
+         */
 		void setIsolatedFamilies(vector<IsolatedFamily> &isolatedFamilies);
     
+        /**
+         * @brief Get the list of products in the input. 
+         * @return A list of products. 
+         */
         vector<Product> &getProducts();
+
+        /**
+         * @brief Get the list of clients in the input. 
+         * @return A list of clients. 
+         */
         vector<Client> getClients();
+
+        /**
+         * @brief Get the list of orders in the input. 
+         * @return A list of orders. 
+         */
         vector<Order> &getOrders();
+
+        /**
+         * @brief Get the list of parameters in the input.
+         * @return A list of parameters. 
+         */
         vector<Parameter> getParameters();
+
+        /**
+		 * @brief Get the list of isolated families in the input.
+		 * @return A list of isolated famlies.
+         */
 		vector<IsolatedFamily> getIsolatedFamily();
+
+        /**
+		 * @brief Get a list of allocation prohibitions in the input.
+		 * @return A list of allocation prohibitions. 
+         */
 		vector<ProductAllocationProhibitions> getAllocationProhibitions(); 
-        Warehouse & getWarehouse();
-    
+
+        /**
+         * @brief Get the input warehouse layout. 
+         * @return A warehouse layout object. 
+         */
+        Warehouse& getWarehouse();  
 };
