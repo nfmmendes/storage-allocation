3f813bedde33e84cc03de13c0e351d2b86cfc41c
nfmmendes <nilson.fmmendes@outlook.com>
2024-08-26 18:30:46 +0200

    Document class NeighborhoodStructure

--- MainSource/NeighborhoodStructure.h
+++ MainSource/NeighborhoodStructure.h
@@ -1,29 +1,58 @@
-#ifndef NEIGHBORHOOD_STRUCTURE_H
-#define NEIGHBORHOOD_STRUCTURE_H
+#pragma once
 
 #include<vector>
 #include "AbstractSolution.h"
 #include "OptimizationConstraints.h"
 
+/**
+ * @brief Class <c> NeighborhoodStructure </c> is an abstract class used to represent
+ * a method to find solutions close to the another solution given in a heuristic method.
+ */
 class NeighborhoodStructure{
 
     protected:
-        AbstractSolution * startSolution;
+        AbstractSolution * startSolution {nullptr};
         
     public:
-        NeighborhoodStructure(){ this->startSolution = NULL; }
+        /** 
+         * @brief Constructor.
+         */
+        NeighborhoodStructure(){ }
+
+        /**
+         * @brief Destructor.
+         */
         virtual ~NeighborhoodStructure(){}
+
+        /**
+         * @brief Constructor.
+         * @param solution Initial solution.
+         */
         NeighborhoodStructure(AbstractSolution *solution) { this->startSolution = solution; }
+
+        /**
+         * @brief Set the initial solution. 
+         * @param solution The initial solution.
+         */
         void setStartSolution(AbstractSolution *solution) {  
-            if(this->startSolution != nullptr){
-               // delete this->startSolution; 
-            }
             this->startSolution = solution; 
         }
+
+        /**
+         * @brief Set optimization/neighborhood constraints. 
+         * @param cons Optimization/neighborhood constraints. 
+         */
         virtual void setOptimizationConstraints(OptimizationConstraints * cons) = 0;
+
+        /**
+         * @brief Get the initial solution.
+         * @return The initial solution.
+         */
         virtual AbstractSolution * getStartSolution() const = 0; 
-        virtual std::vector<AbstractSolution *> createNeighbors() =0;    
 
+        /**
+         * @brief Create a list of neighbors from the initial solution. 
+         * @return A list of solutions. 
+         */
+        virtual vector<AbstractSolution *> createNeighbors() =0;    
 };
-
-#endif
