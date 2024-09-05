cbcc078871f821bee3a03d45596b98412744786b
nfmmendes <nilson.fmmendes@outlook.com>
2024-09-05 11:49:30 +0200

    Add Cell operators tests

--- MainSource/tests/testCell.cpp
+++ MainSource/tests/testCell.cpp
@@ -35,3 +35,77 @@ TEST(TestCell, CellCopyConstructor_Test){
     EXPECT_EQ(a.getRow(), b.getRow()); 
     EXPECT_EQ(a.getColumn(), b.getColumn()); 
 }
+
+TEST(TestCell, CellEqualityOperator_Test){
+    Cell a("A", 2, 3, 5, 4);
+    Cell b("A", 2, 3, 5, 4);
+    Cell c("B", 2, 3, 5, 4);
+    Cell d("A", 8, 3, 5, 4);
+    Cell e("A", 2, 8, 5, 4);
+    Cell f("A", 2, 3, 7, 4);
+    Cell g("A", 2, 3, 5, 8);
+
+    EXPECT_TRUE(a == b);
+    EXPECT_FALSE(a == c);
+    EXPECT_FALSE(a == d);
+    EXPECT_FALSE(a == e);
+    EXPECT_FALSE(a == f);
+    EXPECT_FALSE(a == g);
+}
+
+TEST(TestCell, CellTestAssignmentOperator_Test){
+    Cell a("A", 2, 3, 5, 4);
+    Cell b = a;
+}
+
+TEST(TestCell, CellLessThanOperatorEqualValues_Test){
+    Cell a("A", 2, 3, 5, 4);
+    Cell b("A", 2, 3, 5, 4);
+
+    EXPECT_FALSE(a < b);
+}
+
+TEST(TestCell, CellLessThanOperatorDifferentCodes_Test){
+    Cell a("B", 2, 3, 5, 4);
+    Cell b("A", 2, 3, 5, 4);
+    Cell c("C", 2, 3, 5, 4);
+
+    EXPECT_FALSE(a < b);
+    EXPECT_TRUE(a < c);
+}
+
+TEST(TestCell, CellLessThanOperatorDifferentIdShelves_Test){
+    Cell a("A", 2, 3, 5, 4);
+    Cell b("A", 1, 3, 5, 4);
+    Cell c("A", 3, 3, 5, 4);
+
+    EXPECT_FALSE(a < b);
+    EXPECT_TRUE(a < c);
+}
+
+TEST(TestCell, CellLessThanOperatorDifferentRows_Test){
+    Cell a("A", 2, 4, 5, 4);
+    Cell b("A", 2, 1, 5, 4);
+    Cell c("A", 2, 5, 5, 4);
+
+    EXPECT_FALSE(a < b);
+    EXPECT_TRUE(a < c);
+}
+
+TEST(TestCell, CellLessThanOperatorDifferentColums_Test){
+    Cell a("A", 2, 3, 2, 4);
+    Cell b("A", 2, 3, 1, 4);
+    Cell c("A", 2, 3, 3, 4);
+
+    EXPECT_FALSE(a < b);
+    EXPECT_TRUE(a < c);
+}
+
+TEST(TestCell, CellLessThanOperatorDifferentLevels_Test){
+    Cell a("A", 2, 3, 5, 2);
+    Cell b("A", 2, 3, 5, 1);
+    Cell c("A", 2, 3, 5, 3);
+
+    EXPECT_FALSE(a < b);
+    EXPECT_TRUE(a < c);
+}
