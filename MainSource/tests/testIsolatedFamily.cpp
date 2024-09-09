3967f80be3a87e7f96d6ce20b8784573b864d69d
nfmmendes <nilson.fmmendes@outlook.com>
2024-09-09 17:32:04 +0200

    Add IsolatedFamily operators override tests

--- MainSource/tests/testIsolatedFamily.cpp
+++ MainSource/tests/testIsolatedFamily.cpp
@@ -26,3 +26,32 @@ TEST(TestIsolatedFamily, IsolatedFamilyCopyConstructor_Test){
     EXPECT_EQ(a.getForce(), b.getForce());
     EXPECT_EQ(a.getLevel(), b.getLevel());
 }
+
+TEST(TestIsolatedFamily, IsolatedFamilyAssignmentOperator_Test){
+    IsolatedFamily a("Force", "Level", "Code");
+    IsolatedFamily b("No", "Pi", "Delta"); 
+    
+    //Test pre conditions.
+    EXPECT_NE(a.getCode(), b.getCode());
+    EXPECT_NE(a.getForce(), b.getForce());
+    EXPECT_NE(a.getLevel(), b.getLevel());
+    
+    b = a;
+
+    EXPECT_EQ(a.getCode(), b.getCode());
+    EXPECT_EQ(a.getForce(), b.getForce());
+    EXPECT_EQ(a.getLevel(), b.getLevel());
+}
+
+TEST(TestIsolatedFamily, IsolatedFamilyEqualityOperator_Test){
+    IsolatedFamily a("Force", "Level", "Code");
+    IsolatedFamily b("Force", "Level", "Code");
+    IsolatedFamily c("X", "Level", "Code");
+    IsolatedFamily d("Force", "X", "Code");
+    IsolatedFamily e("Force", "Level", "X");
+
+    EXPECT_TRUE(a == b);
+    EXPECT_FALSE(a == c);
+    EXPECT_FALSE(a == d);
+    EXPECT_FALSE(a == e);
+}
