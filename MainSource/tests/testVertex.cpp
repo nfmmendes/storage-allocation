839a0b51f04eff54788f4ae2c5d20e58f24a8a2a
nfmmendes <nilson.fmmendes@outlook.com>
2024-09-09 16:18:03 +0200

    Implement Vertex setLabel and setType tests

--- MainSource/tests/testVertex.cpp
+++ MainSource/tests/testVertex.cpp
@@ -27,3 +27,29 @@ TEST(TestVertex, VertexCopyConstructor_Test){
     EXPECT_EQ(a.getLabel(), "LABEL", b.getLabel());
     EXPECT_EQ(a.getType(), "TYPE", b.getType());
 }
+
+TEST(TestVertex, VertexSetLabel_Test) { 
+    Vertex a("LABEL", "TYPE", 10.4);
+
+    string newLabel = "NEW_LABEL";
+
+    // Test pre-condition
+    EXPECT_NE(a.getLabel(), newLabel);
+
+    a.setLabel(newLabel);
+
+    EXPECT_EQ(a.getLabel(), newLabel);
+}
+
+TEST(TestVertex, VertexSetTYPE_Test) { 
+    Vertex a("LABEL", "TYPE", 10.4);
+
+    string newType = "NEW_TYPE";
+
+    // Test pre-condition
+    EXPECT_NE(a.getType(), newType);
+
+    a.setType(newType);
+
+    EXPECT_EQ(a.getType(), newType);
+}
