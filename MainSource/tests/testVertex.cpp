9f60ba55f987ae5f69f7739baf384cfedd1b24c8
nfmmendes <nilson.fmmendes@outlook.com>
2024-09-09 16:40:16 +0200

    Implement Vertex operator tests

--- MainSource/tests/testVertex.cpp
+++ MainSource/tests/testVertex.cpp
@@ -24,8 +24,8 @@ TEST(TestVertex, VertexCopyConstructor_Test){
     Vertex b { a };
 
     EXPECT_DOUBLE_EQ(a.getValue(), b.getValue());
-    EXPECT_EQ(a.getLabel(), "LABEL", b.getLabel());
-    EXPECT_EQ(a.getType(), "TYPE", b.getType());
+    EXPECT_EQ(a.getLabel(), b.getLabel());
+    EXPECT_EQ(a.getType(), b.getType());
 }
 
 TEST(TestVertex, VertexSetLabel_Test) { 
@@ -52,4 +52,70 @@ TEST(TestVertex, VertexSetTYPE_Test) {
     a.setType(newType);
 
     EXPECT_EQ(a.getType(), newType);
+}
+
+TEST(TestVertex, VertexEqualityOperator_Test){
+    Vertex a("A", "T_A", 10.0);
+    Vertex b("A", "T_A", 10.0);
+    Vertex c("B", "T_A", 10.0);
+    Vertex d("A", "T_B", 10.0);
+    Vertex e("A", "T_A", 10.2);
+    Vertex f("A", "T_A", 10.000000000002);
+
+    EXPECT_TRUE(a == b);
+    EXPECT_FALSE(a == c);
+    EXPECT_FALSE(a == d);
+    EXPECT_FALSE(a == e);
+    EXPECT_TRUE(a == f);
+}
+
+TEST(TestVertex, VertexInequalityOperator_Test){
+    Vertex a("A", "T_A", 10.0);
+    Vertex b("A", "T_A", 10.0);
+    Vertex c("B", "T_A", 10.0);
+    Vertex d("A", "T_B", 10.0);
+    Vertex e("A", "T_A", 10.2);
+    Vertex f("A", "T_A", 10.000000000002);
+
+    EXPECT_FALSE(a != b);
+    EXPECT_TRUE(a != c);
+    EXPECT_TRUE(a != d);
+    EXPECT_TRUE(a != e);
+    EXPECT_FALSE(a != f);
+}
+
+TEST(TestVertex, VertexLessThanOperator_Test){
+    Vertex a("B", "T_B", 10.0);
+    Vertex b("B", "T_B", 10.0);
+    Vertex c("C", "T_B", 10.0);
+    Vertex d("B", "T_C", 10.0);
+    Vertex e("B", "T_B", 10.2);
+    Vertex f("A", "T_B", 10.0);
+    Vertex g("B", "T_A", 10.0);
+    Vertex h("B", "T_B", 9.2);
+
+    EXPECT_FALSE( a < b);
+    EXPECT_TRUE(a < c);
+    EXPECT_TRUE( a < d);
+    EXPECT_TRUE( a < e);
+    EXPECT_FALSE( a < f);
+    EXPECT_FALSE(a < g);
+    EXPECT_FALSE( a < h);
+}
+
+
+TEST(TestVertex, VertexAssingmentOperator_Test){
+    Vertex a("B", "T_B", 10.0);
+    Vertex b("A", "T_X", 9.0);
+
+    // Test pre conditions.
+    EXPECT_NE(a.getValue(), b.getValue());
+    EXPECT_NE(a.getLabel(), b.getLabel());
+    EXPECT_NE(a.getType(), b.getType());
+
+    b = a;
+
+    EXPECT_DOUBLE_EQ(a.getValue(), b.getValue());
+    EXPECT_EQ(a.getLabel(), b.getLabel());
+    EXPECT_EQ(a.getType(), b.getType());
 }
